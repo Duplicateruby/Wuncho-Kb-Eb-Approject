c726594951b90d3b88af71726c3ac3201888cd24
kaire_bernier <kairebx@gmail.com>
2017-03-16 08:12:12 -0400

    Added the Ball class as well as the Panel class from a previous ball orientated project

--- Wuncho-Kb-Eb-Approject/src/Main.java
+++ Wuncho-Kb-Eb-Approject/src/Main.java
@@ -1,7 +1,7 @@
 /**
  * Created by kaire_bernier on 3/15/17.
  */
-public class Main {
+public class Main extends MyPanel {
     public static void main(String[] args) {
         System.out.println("Git Buds");
 
